fa8351040b8edd6b64126e07096eb670aaf15b5a
prl900 <pablo.larraondo@anu.edu.au>
2018-06-06 23:40:17 +1000

    New and Open functions added

--- geowarp.go
+++ geowarp.go
@@ -2,8 +2,13 @@ package geowarp
 
 import (
 	"errors"
+	"fmt"
+	"github.com/terrascope/gocog"
 	"github.com/terrascope/proj4go"
 	"github.com/terrascope/scimage"
+	"image"
+	//"net/url"
+	"os"
 )
 
 type Position struct {
@@ -12,9 +17,41 @@ type Position struct {
 
 type Raster interface {
 	IsInBounds(int, int) error
-	GetIndex(float64, float64, []float64) (int, int)
-	GetLocation(int, int, []float64) (float64, float64)
+	GetIndex(float64, float64) (int, int)
+	GetLocation(int, int) (float64, float64)
 	Warp(Raster) error
+}
+
+func New(rect image.Rectangle, min, max int16, proj4 string, geot []float64) (Raster, error) {
+	return &GrayGeoRasterS16{GrayS16: scimage.NewGrayS16(rect, min, max), Proj4: proj4, GeoTrans: geot}, nil
+}
+
+func Open(path string) (Raster, error) {
+	//if _, err := url.ParseRequestURI(toTest)
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	info, err := gocog.GeoTIFFInfo(f)
+	if err != nil {
+		return nil, err
+	}
+	f.Close()
+
+	f, err = os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	img, err := gocog.Decode(f)
+	if err != nil {
+		return nil, err
+	}
+	f.Close()
+
+	return &GrayGeoRasterS16{GrayS16: img.(*scimage.GrayS16), Proj4: info.Proj4, GeoTrans: info.Geotransform[:]}, nil
+
 }
 
 var OutOfBoundsError = errors.New("out of bounds")
@@ -45,28 +82,33 @@ func (gr *GrayGeoRasterS16) GetLocation(i, j int) (float64, float64) {
 	return gr.GeoTrans[0] + (gr.GeoTrans[1] * float64(i)), gr.GeoTrans[3] + (gr.GeoTrans[5] * float64(j))
 }
 
-func (gr *GrayGeoRasterS16) Warp(dst GrayGeoRasterS16) error {
-	pixPoints := make([]proj4go.Point, dst.Bounds().Dx()*dst.Bounds().Dy())
-	for i := 0; i < dst.Bounds().Dx(); i++ {
-		for j := 0; j < dst.Bounds().Dy(); j++ {
-			pixPoints[i+j*dst.Bounds().Dx()].X, pixPoints[i+j*dst.Bounds().Dx()].Y = dst.GetLocation(i, j)
+func (gr *GrayGeoRasterS16) Warp(dst Raster) error {
+	dstS16, ok := dst.(*GrayGeoRasterS16)
+	if !ok {
+		return fmt.Errorf("dst has to be a GrayGeoRasterS16 type")
+	}
+
+	pixPoints := make([]proj4go.Point, dstS16.Bounds().Dx()*dstS16.Bounds().Dy())
+	for i := 0; i < dstS16.Bounds().Dx(); i++ {
+		for j := 0; j < dstS16.Bounds().Dy(); j++ {
+			pixPoints[i+j*dstS16.Bounds().Dx()].X, pixPoints[i+j*dstS16.Bounds().Dx()].Y = dstS16.GetLocation(i, j)
 		}
 	}
 
-	err := proj4go.Transform(gr.Proj4, dst.Proj4, pixPoints)
+	err := proj4go.Transform(gr.Proj4, dstS16.Proj4, pixPoints)
 	if err != nil {
 		return err
 	}
 
-	pixLocations := make([]Position, dst.Bounds().Dx()*dst.Bounds().Dy())
+	pixLocations := make([]Position, dstS16.Bounds().Dx()*dstS16.Bounds().Dy())
 	for i, pt := range pixPoints {
 		pixLocations[i].i, pixLocations[i].j = gr.GetIndex(pt.X, pt.Y)
 	}
 
 	for i, loc := range pixLocations {
 		if err := gr.IsInBounds(loc.i, loc.j); err == nil {
-			dst.Pix[2*i] = gr.Pix[loc.i*2+loc.j*2*gr.Bounds().Dx()]
-			dst.Pix[2*i+1] = gr.Pix[loc.i*2+loc.j*2*gr.Bounds().Dx()+1]
+			dstS16.Pix[2*i] = gr.Pix[loc.i*2+loc.j*2*gr.Bounds().Dx()]
+			dstS16.Pix[2*i+1] = gr.Pix[loc.i*2+loc.j*2*gr.Bounds().Dx()+1]
 		}
 	}
 
