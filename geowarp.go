af41d7bc5c2cfe8027dea6031c831f819e7e9b92
prl900 <pablo.larraondo@anu.edu.au>
2018-06-06 22:07:41 +1000

    Basic warping functionality for intS16

--- geowarp.go
+++ geowarp.go
@@ -2,9 +2,9 @@ package geowarp
 
 import (
 	"errors"
-	"github.com/prl900/proj4go"
+	"github.com/terrascope/gocog"
+	"github.com/terrascope/proj4go"
 	"github.com/terrascope/scimage"
-	"image"
 )
 
 type Position struct {
@@ -20,67 +20,14 @@ type Raster interface {
 
 var OutOfBoundsError = errors.New("out of bounds")
 
-type GrayGeoRaster struct {
-	*image.Gray
-	Proj4    string
-	GeoTrans []float64
-	NoData   float64
-}
-
-func (gr *GrayGeoRaster) IsInBounds(i, j int) error {
-	if i < 0 || j < 0 {
-		return OutOfBoundsError
-	}
-	if i >= gr.Bounds().Dx() || j >= gr.Bounds().Dy() {
-		return OutOfBoundsError
-	}
-
-	return nil
-}
-
-func (gr *GrayGeoRaster) GetIndex(x, y float64) (int, int) {
-	return int(((x - gr.GeoTrans[0]) / gr.GeoTrans[1]) + .5), int(((y - gr.GeoTrans[3]) / gr.GeoTrans[5]) + .5)
-}
-
-func (gr *GrayGeoRaster) GetLocation(i, j int) (float64, float64) {
-	return gr.GeoTrans[0] + (gr.GeoTrans[1] * float64(i)), gr.GeoTrans[3] + (gr.GeoTrans[5] * float64(j))
-}
-
-func (gr *GrayGeoRaster) Warp(dst GrayGeoRaster) error {
-	pixPoints := make([]proj4go.Point, dst.Bounds().Dx()*dst.Bounds().Dy())
-	for i := 0; i < dst.Bounds().Dx(); i++ {
-		for j := 0; j < dst.Bounds().Dy(); j++ {
-			pixPoints[i+j*dst.Bounds().Dx()].X, pixPoints[i+j*dst.Bounds().Dx()].Y = dst.GetLocation(i, j)
-		}
-	}
-
-	err := proj4go.Forwards(gr.Proj4, pixPoints)
-	if err != nil {
-		return err
-	}
-
-	pixLocations := make([]Position, dst.Bounds().Dx()*dst.Bounds().Dy())
-	for i, pt := range pixPoints {
-		pixLocations[i].i, pixLocations[i].j = gr.GetIndex(pt.X, pt.Y)
-	}
-
-	for i, loc := range pixLocations {
-		if err := gr.IsInBounds(loc.i, loc.j); err == nil {
-			dst.Pix[i] = gr.Pix[loc.i+loc.j*gr.Bounds().Dx()]
-		}
-	}
-
-	return nil
-}
-
-type GrayGeoRaster16 struct {
+type GrayGeoRasterS16 struct {
 	*scimage.GrayS16
 	Proj4    string
 	GeoTrans []float64
 	NoData   float64
 }
 
-func (gr *GrayGeoRaster16) IsInBounds(i, j int) error {
+func (gr *GrayGeoRasterS16) IsInBounds(i, j int) error {
 	if i < 0 || j < 0 {
 		return OutOfBoundsError
 	}
@@ -91,15 +38,15 @@ func (gr *GrayGeoRaster16) IsInBounds(i, j int) error {
 	return nil
 }
 
-func (gr *GrayGeoRaster16) GetIndex(x, y float64) (int, int) {
+func (gr *GrayGeoRasterS16) GetIndex(x, y float64) (int, int) {
 	return int(((x - gr.GeoTrans[0]) / gr.GeoTrans[1]) + .5), int(((y - gr.GeoTrans[3]) / gr.GeoTrans[5]) + .5)
 }
 
-func (gr *GrayGeoRaster16) GetLocation(i, j int) (float64, float64) {
+func (gr *GrayGeoRasterS16) GetLocation(i, j int) (float64, float64) {
 	return gr.GeoTrans[0] + (gr.GeoTrans[1] * float64(i)), gr.GeoTrans[3] + (gr.GeoTrans[5] * float64(j))
 }
 
-func (gr *GrayGeoRaster16) Warp(dst GrayGeoRaster16) error {
+func (gr *GrayGeoRasterS16) Warp(dst GrayGeoRasterS16) error {
 	pixPoints := make([]proj4go.Point, dst.Bounds().Dx()*dst.Bounds().Dy())
 	for i := 0; i < dst.Bounds().Dx(); i++ {
 		for j := 0; j < dst.Bounds().Dy(); j++ {
@@ -107,7 +54,7 @@ func (gr *GrayGeoRaster16) Warp(dst GrayGeoRaster16) error {
 		}
 	}
 
-	err := proj4go.Forwards(gr.Proj4, pixPoints)
+	err := proj4go.Transform(gr.Proj4, dst.Proj4, pixPoints)
 	if err != nil {
 		return err
 	}
